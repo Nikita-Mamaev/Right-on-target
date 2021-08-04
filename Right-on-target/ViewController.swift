b5a5ce51c8504ecb4716c054286f53386710f70d
Nikita-Mamaev <nikratos@yandex.ru>
2021-08-04 23:19:36 +0300

    Initial Commit

---
+++ Right-on-target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Right-on-target
+//
+//  Created by Nikita Mamaev on 04.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
