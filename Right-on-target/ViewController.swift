b85a900c3953d7b34d96aef1d1c4939159c0f5e8
Nikita-Mamaev <nikratos@yandex.ru>
2021-08-07 00:51:57 +0300

    feat: complete game

--- Right-on-target/ViewController.swift
+++ Right-on-target/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var label: UILabel!
     
@@ -16,13 +16,43 @@ class ViewController: UIViewController {
     var round: Int = 0
     var points: Int = 0
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func checkButtonPressed(_ sender: UIButton) {
+        checkNumber()
+    }
+    
     //Логика игры
     func checkNumber() {
-        
+        if round == 0{
+            number = Int.random(in: 1...50)
+            label.text = String(number)
+            round = 1
+        } else {
+            let numSlider = Int(slider.value.rounded())
+            if numSlider > number {
+                points += 50 - numSlider + number
+            } else if numSlider < number {
+                self.points += 50 - number + numSlider
+            } else {
+                points += 50
+            }
+            if self.round == 5 {
+                let alert = UIAlertController(
+                    title: "Игра окончена",
+                    message: "Вы заработали \(self.points) очков", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+                round = 1
+                points = 0
+            } else {
+                round += 1
+            }
+            number = Int.random(in: 1...50)
+            label.text = String(self.number)
+        }
     }
 }
