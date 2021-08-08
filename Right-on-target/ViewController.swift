d80a3693a1152dcf7ba08d1db5ac84a6545f8927
Nikita-Mamaev <nikratos@yandex.ru>
2021-08-08 23:36:50 +0300

    feat: add life cycle function and launching the game immediately after starting the application

--- Right-on-target/ViewController.swift
+++ Right-on-target/ViewController.swift
@@ -13,11 +13,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     
     var number: Int = 0
-    var round: Int = 0
+    var round: Int = 1
     var points: Int = 0
+    
+    override func loadView() {
+        super.loadView()
+        print("LoadView")
+        let versionLabel = UILabel(frame: CGRect(x: 20, y: 10, width: 200, height: 20))
+        versionLabel.text = "Версия 1.1"
+        self.view.addSubview(versionLabel)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewDidLoad")
+        number = Int.random(in: 1...50)
+        label.text = String(number)
         // Do any additional setup after loading the view.
     }
     
@@ -27,32 +57,27 @@ class ViewController: UIViewController {
     
     //Логика игры
     func checkNumber() {
-        if round == 0{
-            number = Int.random(in: 1...50)
-            label.text = String(number)
-            round = 1
+        let numSlider = Int(slider.value.rounded())
+        if numSlider > number {
+            points += 50 - numSlider + number
+        } else if numSlider < number {
+            self.points += 50 - number + numSlider
         } else {
-            let numSlider = Int(slider.value.rounded())
-            if numSlider > number {
-                points += 50 - numSlider + number
-            } else if numSlider < number {
-                self.points += 50 - number + numSlider
-            } else {
-                points += 50
-            }
-            if self.round == 5 {
-                let alert = UIAlertController(
-                    title: "Игра окончена",
-                    message: "Вы заработали \(self.points) очков", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
-                present(alert, animated: true, completion: nil)
-                round = 1
-                points = 0
-            } else {
-                round += 1
-            }
-            number = Int.random(in: 1...50)
-            label.text = String(self.number)
+            points += 50
         }
+        if self.round == 5 {
+            let alert = UIAlertController(
+                title: "Игра окончена",
+                message: "Вы заработали \(self.points) очков", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            round = 1
+            points = 0
+        } else {
+            round += 1
+        }
+        number = Int.random(in: 1...50)
+        label.text = String(self.number)
+        
     }
 }
